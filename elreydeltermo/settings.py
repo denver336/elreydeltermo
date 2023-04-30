6aedb1e658089c18644fa1eb279928f2c00fb800
Eduardo Cano <canoeduardojesus@gmail.com>
2023-04-30 18:58:48 -0300

    agrecgo csrf

--- elreydeltermo/settings.py
+++ elreydeltermo/settings.py
@@ -137,3 +137,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+CSRF_TRUSTED_ORIGINS = [config('ALLOWED_HOSTS')]
